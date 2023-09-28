764e9533e8199beb162d2daa849f8660bd09685f
Prasenjit Kaninde <31060173+prasenjit-26@users.noreply.github.com>
2023-09-28 18:18:24 +0530

    commit hooks and env sample file

---
+++ .eslintrc.ts
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-var-requires': 0,
+  },
+};
